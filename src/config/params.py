74c7b35d89ec3899ae8fce8f4d12f5305c4ee8a7
edebaze <32875209+edebaze@users.noreply.github.com>
2021-05-25 21:21:11 +0200

    Add files via upload

--- src/config/params.py
+++ src/config/params.py
@@ -1,3 +1,5 @@
+import os, shutil
+
 def get_model_params():
     return {
         'transfer_learning_model_name': 'imagenet',
@@ -10,7 +12,10 @@ def get_model_params():
         # TRAINING PARAMS
         'steps_per_epoch': 200,
         'validation_steps': 50,
-        'epochs': 50,
+        'epochs': 1,
+
+        # OTHER
+        'model_dir': 'models'
     }
 
 
@@ -40,3 +45,15 @@ def get_image_data_generator_params():
         'zoom_range': [1.0, 1.2],
         'validation_split':  0.1
     }
+
+
+def clean_dir(dir_path: str):
+    for filename in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
